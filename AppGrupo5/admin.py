ea7fa41c507f57d59b723c584add6fba23b7be3e
Franco De Nicola <francodenicola98@gmail.com>
2022-01-19 12:54:03 -0300

    Ultimo Commit preentrega

--- AppGrupo5/admin.py
+++ AppGrupo5/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from AppGrupo5.models import Instrumento,Pedal,Discos
+
 
 # Register your models here.
+admin.site.register(Instrumento)
+admin.site.register(Pedal)
+admin.site.register(Discos)
+
--- AppGrupo5/urls.py
+++ AppGrupo5/urls.py
@@ -4,9 +4,9 @@ from AppGrupo5 import views
 
 urlpatterns = [
     
-    path('instrumentos',views.instrumentos),
-    path('pedal', views.pedal),
-    path('disco',views.disco),
+    path('instrumentos',views.instrumentos, name="instrumentos"),
+    path('pedal', views.pedal, name= "pedal"),
+    path('disco',views.disco, name= "disco"),
     path('', views.inicio, name="inicio"),
 ]
 
